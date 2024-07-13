8431b8ace43328b54f9eca5be3f5476623cf2b13
Faozil <85395336+Faozil@users.noreply.github.com>
2024-07-13 17:58:15 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,14 @@
+from flask import Flask 
+from urllib.parse import quote 
 
+def create_app():
+    app = Flask(__name__)
+
+    @app.route('/')
+    def home():
+        return 'Testing my flask app, is this working correctly'
+
+    return app
+if __name__ == '__main__':
+    app = create_app()
+    app.run(host='0.0.0.0', port=80, debug=True)
