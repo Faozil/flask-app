9d3103bfddef42e4f71d139c7f53ba753ea53824
Faozil <85395336+Faozil@users.noreply.github.com>
2024-07-13 18:04:57 +0100

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -15,5 +15,5 @@ def client(app):
 def test_home(client):
     response = client.get('/')
     assert response.status_code == 200
-    expected_text = 'Flask app her,  are you really working?'
+    expected_text = 'Flask app here,  are you really working?'
     assert expected_text.encode() in response.data
